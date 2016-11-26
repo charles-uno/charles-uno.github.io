ddc88463db9e034eb2ec8ff4bf9b4d4472f7f4d4
Charles <charles.a.mceachern@gmail.com>
2016-11-25 23:45:24 -0600

    cleaned up header. UP and DOWN are stand-ins for NAME and DATE

--- assets/_js/sorting.js
+++ assets/_js/sorting.js
@@ -73,10 +73,28 @@ function alphabetical() {
   });
 }
 
+function reversealphabetical() {
+  themes.sort(function(a, b){
+    var titleA = a.title.toLowerCase();
+    var titleB = b.title.toLowerCase();
+    if(titleA > titleB) return -1;
+    if(titleA < titleB) return 1;
+    return 0;
+  });
+}
+
 function latest() {
   themes.sort(function(a, b){
     if(a.date < b.date) return 1;
     if(a.date > b.date) return -1;
+    return 0;
+  });
+}
+
+function reverselatest() {
+  themes.sort(function(a, b){
+    if(a.date > b.date) return 1;
+    if(a.date < b.date) return -1;
     return 0;
   });
 }
