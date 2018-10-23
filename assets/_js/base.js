bfb2139ecfa5b50386fa6887a5c4e74f8b249fe0
Charles <charles.a.mceachern@gmail.com>
2018-10-22 22:01:53 -0500

    not sure what changed, but shuffle seems fixed

--- assets/_js/base.js
+++ assets/_js/base.js
@@ -2,46 +2,48 @@
 
 var posts = [
     {% for post in site.posts %}
-        {% unless post.hidden or post.redirect %}
-            {
-                "title": "{{ post.title }}",
-                "hash": "{{ post.title | remove: ' ' | strip_newlines | downcase | md5 }}",
-                "date": "{{ post.date | date: "%F" }}",
-                "thumbnail": "{{ post.image }}",
-                "url": "{{ site.baseurl }}{{ post.url }}",
-                {% assign words = "" | split: "|" %}
-                {% capture content_plus %}{{ post.title }} {{ post.description }} {{ post.content }} {{ post.keywords }}{% endcapture %}
-                {% assign raw_words = content_plus
-                    | strip_html
-                    | downcase
-                    | replace: "\n", " "
-                    | replace: "é", "e"
-                    | remove: "."
-                    | remove: "↩"
-                    | remove: ","
-                    | remove: "?"
-                    | remove: "…"
-                    | remove: '"'
-                    | remove: "!"
-                    | remove: "-"
-                    | remove: "/"
-                    | remove: ":"
-                    | remove: ";"
-                    | remove: "("
-                    | remove: ")"
-                    | split: " "
-                %}
-                // Skip duplicates
-                {% for word in raw_words %}
-                    {% unless words contains word %}
-                        {% assign words = words | push: word %}
-                    {% endunless %}
-                {% endfor %}
-                "words":"{{ words | join: " " }}",
-                "share-url":"{{ post.url | prepend: site.baseurl | prepend: site.url }}",
-                "share-title":"{{ post.title | url_encode }}",
-                "share-excerpt":"{{ post.description | url_encode }}"
-            }{% unless forloop.last %},{% endunless %}
+        {% unless post.hidden %}
+            {% unless post.redirect %}
+                {
+                    "title": "{{ post.title }}",
+                    "hash": "{{ post.title | remove: ' ' | strip_newlines | downcase | md5 }}",
+                    "date": "{{ post.date | date: "%F" }}",
+                    "thumbnail": "{{ post.image }}",
+                    "url": "{{ site.baseurl }}{{ post.url }}",
+                    {% assign words = "" | split: "|" %}
+                    {% capture content_plus %}{{ post.title }} {{ post.description }} {{ post.content }} {{ post.keywords }}{% endcapture %}
+                    {% assign raw_words = content_plus
+                        | strip_html
+                        | downcase
+                        | replace: "\n", " "
+                        | replace: "é", "e"
+                        | remove: "."
+                        | remove: "↩"
+                        | remove: ","
+                        | remove: "?"
+                        | remove: "…"
+                        | remove: '"'
+                        | remove: "!"
+                        | remove: "-"
+                        | remove: "/"
+                        | remove: ":"
+                        | remove: ";"
+                        | remove: "("
+                        | remove: ")"
+                        | split: " "
+                    %}
+                    // Skip duplicates
+                    {% for word in raw_words %}
+                        {% unless words contains word %}
+                            {% assign words = words | push: word %}
+                        {% endunless %}
+                    {% endfor %}
+                    "words":"{{ words | join: " " }}",
+                    "share-url":"{{ post.url | prepend: site.baseurl | prepend: site.url }}",
+                    "share-title":"{{ post.title | url_encode }}",
+                    "share-excerpt":"{{ post.description | url_encode }}"
+                }{% unless forloop.last %},{% endunless %}
+            {% endunless %}
         {% endunless %}
     {% endfor %}
 ];
@@ -57,9 +59,11 @@ var shuffledTitles = [];
 // key, and sort by the keys.
 function initShuffledTitles() {
     var shuffledPosts = [];
+//    console.log('shuffling titles');
     // Assign each title a random sort key.
     for (var i=0; i < posts.length; i++) {
         shuffledPosts.push( {'title':posts[i].title, 'sortkey':Math.random()} );
+//        console.log(posts[i].title);
     }
     // To shuffle, sort by the sortkeys.
     shuffledPosts.sort( function(a, b){ return a.sortkey < b.sortkey; } );
