5c3e5b572d5d6a06ea0abe67238fc3cd2bf39bb1
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2022-02-16 13:28:41 -0600

    tidy up wrappers and tables

--- assets/_js/post.js
+++ assets/_js/post.js
@@ -1,23 +1,4 @@
 
-// Wrap each table in a div so we can round its corners nicely. If the
-// table is immediately followed by a caption, get that in there too.
-var tables = document.getElementsByTagName('table');
-for (var i=0; i<tables.length; i++) {
-    var wrapper = document.createElement("div");
-    wrapper.classList.add("table-wrapper");
-    var next = tables[i].nextElementSibling;
-    // Insert the wrapper right before the table.
-    tables[i].parentNode.insertBefore(wrapper, tables[i]);
-    // Move the table into the wrapper.
-    wrapper.appendChild(tables[i]);
-    // If it's followed by a caption, move that in there as well.
-    if (next.classList && next.classList.contains("table-caption")) {
-        wrapper.appendChild(next);
-    }
-}
-
-// ---------------------------------------------------------------------
-
 // The CSS ::before pseudo-element is not allowed to insert HTML, so
 // let's use some Javascript to insert an <hr> before the div that
 // contains the footnotes.
