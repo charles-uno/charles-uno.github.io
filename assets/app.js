fded7320d259cf5275588b7ad119f81c1184f500
Charles Fyfe <charles.andrew.fyfe@gmail.com>
2022-02-20 10:43:29 -0600

    pre-footnote hr in liquid

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,5 @@
 
 {% include_relative _js/base.js %}
 {% include_relative _js/tags.js %}
-{% include_relative _js/post.js %}
 {% include_relative _js/autocard.js %}
 {% include_relative _js/units.js %}
